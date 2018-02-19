b9864ea50ea80bd29bbb9915cd950286575c353a
Honchar Denys <crackeraki@gmail.com>
2018-02-19 20:08:15 +0200

    fixed links

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.set('view cache', true);
 app.set('views', __dirname + '/html');
 app.get('/', function(req, res){
 	res.render('apps', {
+		nav: {
+			land: 'https://www.bejaboo.com/en',
+			babybook: 'https://www.bejaboo.com/babybook',
+			pricing: 'https://www.bejaboo.com/Pricing',
+			legal: 'https://www.bejaboo.com/Legal',
+			babybook: 'https://www.bejaboo.com/babybook',
+			pregnantbook: 'https://www.bejaboo.com/pregnantbok',
+			membership: 'https://www.bejaboo.com/shop/en',
+			names: 'https://www.bejaboo.com/babynames',
+			howDoesItWork: 'https://www.bejaboo.com/HowDoesItWork/En',
+			login: 'https://www.bejaboo.com/login'
+		},
 		lan: 'en'
 	});
 });
